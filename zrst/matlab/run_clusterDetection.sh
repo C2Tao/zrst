aaa6dc40d6fb83b23f501c8d0d192cde474f0b44
c2tao <b97901182@gmail.com>
2015-10-19 13:10:53 +0800

    path_fix

--- zrst/matlab/run_clusterDetection.sh
+++ zrst/matlab/run_clusterDetection.sh
@@ -1,5 +1,6 @@
 #matlab -nosplash -nowindow -r "path='/home/c2tao/phd/GlobalPhone_subset/German/wav/train/';clusterNumber=50;dumpfile='german50'; clusterDetection_commandline" 
+ORI=$PWD
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 matlab -nosplash -nowindow -r "path='$1';clusterNumber=$2;dumpfile='$3'; clusterDetection_commandline" 
-
+cd $ORI
